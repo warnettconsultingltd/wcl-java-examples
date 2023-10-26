c5cb2686481efef60a85b9742b49a390e444c7b4
wcl-martinwarnett <coding@warnettconsultingltd.co.uk>
2023-10-26 22:44:30 +0100

    Implemented examples for basic Switch pattern.

--- examples-java21/examples-java-21-stable/src/main/java/com/wcl/examples/jep441_switch_pattern_matching/ObjectMatch.java
+++ examples-java21/examples-java-21-stable/src/main/java/com/wcl/examples/jep441_switch_pattern_matching/ObjectMatch.java
@@ -4,9 +4,9 @@ public class ObjectMatch {
     private static final String MATCH_TEXT = "%s passed in";
     public String matchObject(Object obj) {
         return switch (obj) {
-            case Integer intValue -> String.format("%s passed in","Integer");
-            case null -> String.format("%s passed in","Null");
-            default -> String.format("%s passed in",obj.getClass().getName());
+            case Integer intValue -> String.format(MATCH_TEXT,"Integer");
+            case null -> String.format(MATCH_TEXT,"Null");
+            default -> String.format(MATCH_TEXT,obj.getClass().getSimpleName());
         };
     }
 }
