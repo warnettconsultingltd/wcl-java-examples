8bfc5a71b287b4eb8b8e118f0cda3441e811a6a9
wcl-martinwarnett <coding@warnettconsultingltd.co.uk>
2023-10-27 16:41:33 +0100

    Made parameter final.

--- examples-java21/examples-java-21-stable/src/main/java/com/wcl/examples/jep441_switch_pattern_matching/ObjectMatch.java
+++ examples-java21/examples-java-21-stable/src/main/java/com/wcl/examples/jep441_switch_pattern_matching/ObjectMatch.java
@@ -2,7 +2,7 @@ package com.wcl.examples.jep441_switch_pattern_matching;
 
 public class ObjectMatch {
     private static final String MATCH_TEXT = "%s passed in";
-    public String matchObject(Object obj) {
+    public String matchObject(final Object obj) {
         return switch (obj) {
             case Integer intValue -> String.format(MATCH_TEXT,"Integer");
             case null -> String.format(MATCH_TEXT,"Null");
